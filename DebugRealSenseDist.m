45ba6a116c78324d1ae9338f33baa87bce5ec1e0
lirangazit <47790066+lirangazit@users.noreply.github.com>
2020-01-14 10:58:03 -0500

    Update DebugRealSenseDist.m

--- DebugRealSenseDist.m
+++ DebugRealSenseDist.m
@@ -1,18 +1,10 @@
 function depth_array = DebugRealSenseDist(serPort)
 
-% RealSenseDist(serPort, num_points)returns an array of floats, each 
+% DebugRealSenseDist(serPort) returns an array of floats, each 
 % element of [depth_array] represents the depth of the point in meters,
 % from the camera. 
 % Requires:
-%   - [num_points] must be >= 2 and <10, is the number of points to return from 
-%   the camera, taken in regular intervals horizontally across the depth
-%   image. The first element is always the leftmost point of the image (~27
-%   degrees from center), and the last element is always the rightmost point
-%   of the image. 
-%   - [height] is the integer angle in degrees from the bottom of the
-%   depth image, must be in range [1, 40] inclusive. 
-% Note the minimum effective distance for the depth sensor is 0.15m, and
-% the maximum effective distance is ~10 meters. 
+% serPort is a tcp port initialized by DebugPiInit
     
 %Flush Buffer    
 N = serPort.BytesAvailable();
@@ -42,8 +34,6 @@ to_str = char(resp.');
 %split by empty space and convert to array of data
 cell_array = strsplit(to_str, ' ');
 
-%disp('got data!');
-%fprintf('response was: %s\n',resp); % Print response to command window
 depth_array = zeros(num_points, 1);
 for i=1:num_points
     depth_array(i) = str2double(cell_array(i));
